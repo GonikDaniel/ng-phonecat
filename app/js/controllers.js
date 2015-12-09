550aef5c9347e77114ef880f332eaceab098d3fb
Gonik Daniel <gonik.daniel88@gmail.com>
2015-12-09 13:38:58 +0300

    basic usage of controller and data-binding + some unit-tests

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,22 @@
 'use strict';
 
 /* Controllers */
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope){
+    $scope.phones = [
+        {
+            'name': 'Nexus S',
+            'snippet': 'Fast just got faster with Nexus S.'
+        },
+        {
+            'name': 'Motorola XOOM™',
+            'snippet': 'The next, next generation tablet.'
+        },
+        {
+            'name': 'Motorola XOOM™',
+            'snippet': 'The next, next generation tablet.'
+        }
+    ];
+});
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,8 +4,17 @@
 
 describe('controllers', function() {
 
-  it("should do something", function() {
+    describe('PhoneListCtrl', function() {
 
-  });
+        it('should do something', function() {
+            beforeEach(module('phonecatApp'));
 
+            it('should create "phones" model with 4 phones', inject(function($controller) {
+               var scope = {},
+                   ctrl = $controller('PhoneListCtrl', {$scope:scope}) ;
+
+                expect(scope.phones.length).toBe(3);
+            }));
+        });
+    });
 });
